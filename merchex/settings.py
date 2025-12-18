570781ff862587845560762c86a336f7bbc51411
Hortanceleatetia <leatitiaowona1@gmail.com>
2025-12-18 02:44:54 +0100

    Add Procfile

--- merchex/settings.py
+++ merchex/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -79,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    
+        
     }
 }
 

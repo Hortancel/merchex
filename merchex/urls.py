d8d4806a5fcda0fbe4231a27756ba4dd2fcfb427
Hortanceleatetia <leatitiaowona1@gmail.com>
2025-12-18 20:21:21 +0100

    Fix django routes and render deployment

--- merchex/urls.py
+++ merchex/urls.py
@@ -5,12 +5,14 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.band_detail, name='band-detail'),
      path('bands/', views.band_list, name='band-list'),
      path('bands/<int:id>/',views.band_detail, name='band-detail'),
      path('bands/add/' , views.band_create, name='band-create'),
      path('contact-us/' ,views.contact,name='contact'),
      path('bands/delete/<int:id>', views.band_delete, name='band-delete'),
      path('bands/<int:id>/change/',views.band_update,name='band-update'),
+     
    path('about-us/', views.about),
     
 ]
